8eaf64a2a2c32626b367e0e69249d49a45930b1e
lee-to <thecutcode@gmail.com>
2026-01-19 17:31:52 +0300

    fix: Issue
    Issue #6

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,16 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         emptyOutDir: false,
+        lib: {
+            entry: 'resources/js/filepond.js',
+            name: 'MoonshineFilepond',
+            formats: ['iife'],
+            fileName: () => 'filepond.js',
+            cssFileName: 'filepond',
+        },
         rollupOptions: {
-            input: ['resources/js/filepond.js', 'resources/css/filepond.css'],
             output: {
-                entryFileNames: `filepond.js`,
-                assetFileNames: file => {
-                    let ext = file.name.split('.').pop()
-                    if (ext === 'css') {
-                        return 'filepond.css'
-                    }
-
-                    return '[name].[ext]'
-                }
+                assetFileNames: 'filepond.[ext]',
             }
         },
         outDir: 'dist',
